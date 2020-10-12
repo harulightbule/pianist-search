d6d4c9aeb66a08928535dfa7037f96073b7af608
harulightbule <peche.saison.1023@gmail.com>
2020-10-12 16:40:29 +0900

    アソシエーションとバリデーションの設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  has_one :pianist
 
  with_options presence: true do
   validates :name
