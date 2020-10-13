ca4e03760c3cec38b280c8fb578c45e528967bfc
harulightbule <peche.saison.1023@gmail.com>
2020-10-13 16:47:24 +0900

    ルーティングの設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pianists#index'
-  resources :pianists, only: [:index, :new, :create]
+  resources :pianists, only: [:index, :new, :create, :show]
 end
